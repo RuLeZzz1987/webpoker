f412312096608a0f9fa79c8fc902f8c410fe33d4
RuLeZzz1987 <imrunningmyhairwavingback@gmail.com>
2014-06-14 16:15:14 +0300

    engine test

--- web/src/test/java/com/rulezzz/pkr/web/GameEngineTest.java
+++ web/src/test/java/com/rulezzz/pkr/web/GameEngineTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.*;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
@@ -18,22 +19,31 @@ import org.junit.Test;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
+import com.rulezzz.pkr.core.Card;
 import com.rulezzz.pkr.core.GameType;
+import com.rulezzz.pkr.core.Hand;
 import com.rulezzz.pkr.core.Table;
 
 public class GameEngineTest {
     
     @Test
     public void testGameEngineServlet() throws ServletException, IOException {
-    /*    HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
         HttpSession session = mock(HttpSession.class);
         RequestDispatcher reqDispatcher = mock(RequestDispatcher.class);
         Table table = new Table(GameType.FIVECARD);
         table.makeBets(20, 25, 15);
         table.deal();
+        List<Card> handThatDraw = table.getBox(0).getHand().getCards();
         session.setAttribute("table", table);
         
+        when(req.getParameter(handThatDraw.get(0).getStringCard())).thenReturn(null);
+        when(req.getParameter(handThatDraw.get(2).getStringCard())).thenReturn(null);
+        when(req.getParameter(handThatDraw.get(4).getStringCard())).thenReturn(null);
+        when(req.getParameter("choise0")).thenReturn("draw");
+        when(req.getParameter("choise1")).thenReturn("fold");
+        when(req.getParameter("choise2")).thenReturn("bet");
         when(req.getSession()).thenReturn(session);
         when(req.getRequestDispatcher("/WEB-INF/jsp/game.jsp"))
         .thenReturn(reqDispatcher);
@@ -61,7 +71,7 @@ public class GameEngineTest {
         GameEngineServlet engine = new GameEngineServlet();
         engine.doPost(req, resp);
         
-        verify(req, atLeast(1)).getSession(); */
+        verify(req, atLeast(1)).getSession(); 
         
     }
 }
